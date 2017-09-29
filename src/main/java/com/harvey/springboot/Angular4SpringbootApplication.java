6a0a33066db0abbec414795fb4a56cf662a23ca8
Harvey Huang <harvey.huang@oniqua.com>
2017-09-29 21:26:52 +1000

    initial commit angular

---
+++ src/main/java/com/harvey/springboot/Angular4SpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.harvey.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Angular4SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Angular4SpringbootApplication.class, args);
+	}
+}
